3f54beace661c6553ac7c9efcbd16222f3611ca6
Yunus Emre Şahin <yunusemre.sahin@ogr.iu.edu.tr>
2023-06-21 01:38:32 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer
+            {
+                DateOfBirth = new DateTime(2001, 1, 8), FirstName = "Yunus Emre", LastName = "Şahin",
+                NationalityId = "59710386942"
+            });
+            Console.ReadLine();
+
+        }
+    }
+}
